125819696dbcb7e62cf270db5a6d156feaae0ed5
Adham Mamdouh <mamdouhadham57@gmail.com>
2022-04-18 22:50:31 +0200

    Created Initial Chat/Message Model and Controller

---
+++ db/migrate/20220418204722_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[5.1]
+  def change
+    create_table :messages do |t|
+      t.references :chatId, foreign_key: true
+      t.integer :number
+      t.string :content
+
+      t.timestamps
+    end
+  end
+end
