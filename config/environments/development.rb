27e3aaae9cc35e076700c408798b60187c23d186
Adham Mamdouh <mamdouhadham57@gmail.com>
2022-04-22 09:26:40 +0200

    Removed DB Environment Variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  
-  ENV['DB_USER'] = "instabug"
-  ENV['DB_PASSWORD'] = "password"
 end
